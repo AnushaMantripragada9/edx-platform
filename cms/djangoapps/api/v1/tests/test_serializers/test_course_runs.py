536a86643c3627bb43f745ae76860f50802a7cff
rabia23 <rabiaiftikhar2392@gmail.com>
2018-02-27 16:50:34 +0000

    EDUCATOR-2231 remove course run enrollment dates references from studio

--- cms/djangoapps/api/v1/tests/test_serializers/test_course_runs.py
+++ cms/djangoapps/api/v1/tests/test_serializers/test_course_runs.py
@@ -16,39 +16,45 @@ from ...serializers.course_runs import CourseRunSerializer
 @ddt.ddt
 class CourseRunSerializerTests(ModuleStoreTestCase):
 
-    @ddt.data(
-        ('instructor_paced', False),
-        ('self_paced', True),
-    )
-    @ddt.unpack
-    def test_data(self, expected_pacing_type, self_paced):
-        start = datetime.datetime.now(pytz.UTC)
-        end = start + datetime.timedelta(days=30)
-        enrollment_start = start - datetime.timedelta(days=7)
-        enrollment_end = end - datetime.timedelta(days=14)
+    def setUp(self):
+        super(CourseRunSerializerTests, self).setUp()
 
-        course = CourseFactory(
-            start=start,
-            end=end,
-            enrollment_start=enrollment_start,
-            enrollment_end=enrollment_end,
-            self_paced=self_paced
-        )
+        self.start = datetime.datetime.now(pytz.UTC)
+        self.end = self.start + datetime.timedelta(days=30)
+
+        self.request = RequestFactory().get('')
+
+    def get_course(self, self_paced, enrollment_start=None, enrollment_end=None):
+
+        if enrollment_start and enrollment_end:
+            return CourseFactory(
+                start=self.start, end=self.end, self_paced=self_paced, enrollment_start=enrollment_start,
+                enrollment_end=enrollment_end
+            )
+        else:
+            return CourseFactory(start=self.start, end=self.end, self_paced=self_paced)
+
+    def get_course_user_roles(self, course):
+
         instructor = UserFactory()
         CourseInstructorRole(course.id).add_users(instructor)
         staff = UserFactory()
         CourseStaffRole(course.id).add_users(staff)
 
-        request = RequestFactory().get('')
-        serializer = CourseRunSerializer(course, context={'request': request})
-        expected = {
+        return instructor, staff
+
+    def get_expected_data(
+        self, course, start, end, enrollment_start, enrollment_end,
+        instructor, staff, expected_pacing_type
+    ):
+        return {
             'id': str(course.id),
             'title': course.display_name,
             'schedule': {
                 'start': serialize_datetime(start),
                 'end': serialize_datetime(end),
-                'enrollment_start': serialize_datetime(enrollment_start),
-                'enrollment_end': serialize_datetime(enrollment_end),
+                'enrollment_start': enrollment_start,
+                'enrollment_end': enrollment_end,
             },
             'team': [
                 {
@@ -61,8 +67,45 @@ class CourseRunSerializerTests(ModuleStoreTestCase):
                 },
             ],
             'images': {
-                'card_image': request.build_absolute_uri(course_image_url(course)),
+                'card_image': self.request.build_absolute_uri(course_image_url(course)),
             },
             'pacing_type': expected_pacing_type,
         }
+
+    @ddt.data(
+        ('instructor_paced', False),
+        ('self_paced', True),
+    )
+    @ddt.unpack
+    def test_data_with_enrollment_dates(self, expected_pacing_type, self_paced):
+
+        enrollment_start = self.start - datetime.timedelta(days=7)
+        enrollment_end = self.end - datetime.timedelta(days=14)
+        course = self.get_course(self_paced, enrollment_start, enrollment_end)
+        instructor, staff = self.get_course_user_roles(course)
+        serializer = CourseRunSerializer(course, context={'request': self.request})
+
+        expected = self.get_expected_data(
+            course, self.start, self.end, serialize_datetime(enrollment_start),
+            serialize_datetime(enrollment_end), instructor, staff, expected_pacing_type
+        )
+
+        assert serializer.data == expected
+
+    @ddt.data(
+        ('instructor_paced', False),
+        ('self_paced', True),
+    )
+    @ddt.unpack
+    def test_data_without_enrollment_dates(self, expected_pacing_type, self_paced):
+
+        course = self.get_course(self_paced)
+        instructor, staff = self.get_course_user_roles(course)
+        serializer = CourseRunSerializer(course, context={'request': self.request})
+
+        expected = self.get_expected_data(
+            course, self.start, self.end, None, None, instructor, staff,
+            expected_pacing_type
+        )
+
         assert serializer.data == expected
